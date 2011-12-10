c4c4f755bd348be7f14be62dfdde9e8cafd77612
Feildmaster <admin@feildmaster.com>
2011-12-10 06:26:27 -0600

    Adding Libs... Is this worth it?

--- src/feildmaster/anotherTNTplugin/boom.java
+++ src/feildmaster/anotherTNTplugin/boom.java
@@ -1,6 +1,5 @@
 package feildmaster.anotherTNTplugin;
 
-import org.blockface.bukkitstats.CallHome;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,9 +9,6 @@ public class boom extends JavaPlugin {
     }
 
     public void onEnable() {
-        // Stats Usage
-        CallHome.load(this);
-
         getServer().getPluginManager().registerEvent(Event.Type.BLOCK_PLACE, new blockListener(), Event.Priority.Lowest, this);
         getServer().getLogger().info(String.format("[%1$s] v%2$s Enabled!", getDescription().getName(), getDescription().getVersion()));
     }
