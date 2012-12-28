520ecf4919e93c480ee5e852ab90f2dfab6c6313
feildmaster <admin@feildmaster.com>
2012-12-28 17:51:12 -0600

    renamed to damage-blocks

--- src/main/java/com/feildmaster/anotherTNTplugin/BoomPlugin.java
+++ src/main/java/com/feildmaster/anotherTNTplugin/BoomPlugin.java
@@ -7,8 +7,12 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BoomPlugin extends JavaPlugin implements Listener {
+    private boolean damageBlocks;
+
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        reloadConfig();
+        damageBlocks = getConfig().getBoolean("damage-blocks");
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -21,7 +25,7 @@ public class BoomPlugin extends JavaPlugin implements Listener {
                 event.getPlayer().sendMessage("You messed up when setting TNT!");
                 block.setType(Material.AIR);
                 Location l = block.getLocation();
-                event.getPlayer().getWorld().createExplosion(l.getX(), l.getY(), l.getZ(), 4F, getConfig().getBoolean("explode-blocks"), getConfig().getBoolean("explode-blocks"));
+                event.getPlayer().getWorld().createExplosion(l.getX(), l.getY(), l.getZ(), 4F, damageBlocks, damageBlocks);
             }
         }
     }
