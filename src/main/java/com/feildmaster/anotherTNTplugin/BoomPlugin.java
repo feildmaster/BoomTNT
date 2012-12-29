ed44341ed1e227e36cd1f333aef6422b1aa84531
feildmaster <admin@feildmaster.com>
2012-12-28 18:14:19 -0600

    refactor code

--- src/main/java/com/feildmaster/anotherTNTplugin/BoomPlugin.java
+++ src/main/java/com/feildmaster/anotherTNTplugin/BoomPlugin.java
@@ -11,22 +11,28 @@ public class BoomPlugin extends JavaPlugin implements Listener {
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        reloadConfig();
         damageBlocks = getConfig().getBoolean("damage-blocks");
+    }
+
+    public void onDisable() {
+        reloadConfig();
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockPlace(BlockPlaceEvent event) {
-        if(event.getPlayer().getGameMode() == GameMode.CREATIVE || event.getPlayer().hasPermission("boom.bypass")) return;
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE || event.getPlayer().hasPermission("boom.bypass")) {
+            return;
+        }
 
         Block block = event.getBlock();
-        if(block.getType() == Material.TNT) {
-            if(Math.random() < 0.9) {
-                event.getPlayer().sendMessage("You messed up when setting TNT!");
-                block.setType(Material.AIR);
-                Location l = block.getLocation();
-                event.getPlayer().getWorld().createExplosion(l.getX(), l.getY(), l.getZ(), 4F, damageBlocks, damageBlocks);
-            }
+        if (block.getType() != Material.TNT || Math.random() >= 0.9) {
+            return;
         }
+
+        event.getPlayer().sendMessage("You messed up when setting TNT!");
+        block.setType(Material.AIR);
+
+        Location l = block.getLocation();
+        event.getPlayer().getWorld().createExplosion(l.getX(), l.getY(), l.getZ(), 4F, damageBlocks, damageBlocks);
     }
 }
